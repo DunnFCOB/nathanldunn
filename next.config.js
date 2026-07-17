4f3735201d97dc1e004fa3b559a7eef388c2ca0d
nathandunn <ndunn.376@gmail.com>
2026-07-17 19:31:44 -0400

    mx branch: index only

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export", // static export for Vercel/Netlify/GitHub Pages
-  images: {
-    unoptimized: true,
-  },
-};
-
-module.exports = nextConfig;
